c70b93099b787570b7ba98221f29a3d9d8036696
Sharon Arana <shar_mdza@hotmail.com>
2022-10-24 14:13:16 -0500

    Add nav, card and input feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import validator from './validator.js';
+const inputs = document.querySelectorAll(".cardInput");
+inputs.forEach((input, index) => input.onkeyup = () => {
+    const maxLength = input.getAttribute("maxlength");
+    cardValidation(input, maxLength, index)
+})
 
-console.log(validator);
+function cardValidation(input, maxLength, index){
+    if(input.value.length === 4 && index < 3){
+        inputs[index + 1].focus()
+    }else if(input.value.length > 4){
+        input.value = input.value.substring(0, maxLength)
+    }
+}
