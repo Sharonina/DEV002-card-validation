c8c465d6669dc885bffcbb64b8bbe4a5a546a99a
Sharon Arana <shar_mdza@hotmail.com>
2022-10-26 18:14:34 -0500

    add name validation feature, placeholder styles and change on hide and show card numbers feature

--- src/index.js
+++ src/index.js
@@ -19,15 +19,21 @@ const hideNumbersButton = document.querySelector(".hideNumbersButton");
 
 hideNumbersButton.onclick = () => {
     if(hideNumbersButton.innerHTML === 'Ocultar dígitos'){
-        inputs[0].classList.add("hideNumbers");
+        /* inputs[0].classList.add("hideNumbers");
         inputs[1].classList.add("hideNumbers");
-        inputs[2].classList.add("hideNumbers");
+        inputs[2].classList.add("hideNumbers"); */
+        inputs[0].type = "password"
+        inputs[1].type = "password"
+        inputs[2].type = "password"
 
         hideNumbersButton.innerHTML = "Mostrar dígitos"
     }else{
-        inputs[0].classList.remove("hideNumbers");
+        /* inputs[0].classList.remove("hideNumbers");
         inputs[1].classList.remove("hideNumbers");
-        inputs[2].classList.remove("hideNumbers");
+        inputs[2].classList.remove("hideNumbers"); */
+        inputs[0].type = "number"
+        inputs[1].type = "number"
+        inputs[2].type = "number"
 
         hideNumbersButton.innerHTML = "Ocultar dígitos"
     }
@@ -36,51 +42,57 @@ hideNumbersButton.onclick = () => {
 //Validación de tarjeta
 //convertir node list a array de num
 const validateCard = document.querySelector(".validate-card-button");
+const userNameInput = document.querySelector(".user-name-input")
 
 validateCard.onclick = () => {
-    const cardInputNumbers = [];
 
-    let inputsValue = [...inputs]
-    inputsValue.forEach(item => cardInputNumbers.push(item.value))
+    if(userNameInput.value.length == 0){
+        userNameInput.placeholder = "Por favor, ingresa tu nombre"
+    } else {
+        const cardInputNumbers = [];
 
-    const cardNumbers = cardInputNumbers.map(item => item.split(''))
-    let newCardNumbers = [...cardNumbers[0], ...cardNumbers[1], ...cardNumbers[2], ...cardNumbers[3]];
-    const realCardNumbers = newCardNumbers.map(item => parseInt(item))
+        let inputsValue = [...inputs]
+        inputsValue.forEach(item => cardInputNumbers.push(item.value))
 
-//operaciones
-    const validation = realCardNumbers.map((item, index) => {
-        if((index + 1) % 2 === 0){
-            let numberDouble = item * 2
-            if(numberDouble > 9){
-                const numberDobleString = numberDouble.toString()
-                const numberDoubleSum = parseInt(numberDobleString.charAt(0)) + parseInt(numberDobleString.charAt(1))
-                return numberDoubleSum
+        const cardNumbers = cardInputNumbers.map(item => item.split(''))
+        let newCardNumbers = [...cardNumbers[0], ...cardNumbers[1], ...cardNumbers[2], ...cardNumbers[3]];
+        const realCardNumbers = newCardNumbers.map(item => parseInt(item))
+
+    //operaciones
+        const validation = realCardNumbers.map((item, index) => {
+            if((index + 1) % 2 === 0){
+                let numberDouble = item * 2
+                if(numberDouble > 9){
+                    const numberDobleString = numberDouble.toString()
+                    const numberDoubleSum = parseInt(numberDobleString.charAt(0)) + parseInt(numberDobleString.charAt(1))
+                    return numberDoubleSum
+                } else{
+                    return numberDouble
+                }
             } else{
-                return numberDouble
+                return item
             }
+        })
+    //Sumatoria
+        const validationSum = validation.reduce((previo, actual) => previo + actual)
+        console.log(validationSum)
+
+    //condicionales para el mensaje de validacion
+        const validationMessage = document.querySelector(".validation-message");
+        const messageSpan = document.querySelector(".message-span");
+        const iconImg = document.querySelector(".icon-img");
+
+        if(validationSum % 10 === 0){
+            validationMessage.classList.remove("display-off");
+            validateCard.classList.add("display-off")
+            console.log('true')
         } else{
-            return item
+            validationMessage.classList.remove("display-off");
+            messageSpan.innerHTML = "¡Lo sentimos! Tu tarjeta es invalida"
+            validateCard.classList.add("display-off")
+            iconImg.src = "imgs/x-mark.png"
+            console.log('false')
         }
-    })
-//Sumatoria
-    const validationSum = validation.reduce((previo, actual) => previo + actual)
-    console.log(validationSum)
-
-//condicionales para el mensaje de validacion
-    const validationMessage = document.querySelector(".validation-message");
-    const messageSpan = document.querySelector(".message-span");
-    const iconImg = document.querySelector(".icon-img");
-
-    if(validationSum % 10 === 0){
-        validationMessage.classList.remove("display-off");
-        validateCard.classList.add("display-off")
-        console.log('true')
-    } else{
-        validationMessage.classList.remove("display-off");
-        messageSpan.innerHTML = "¡Lo sentimos! Tu tarjeta es invalida"
-        validateCard.classList.add("display-off")
-        iconImg.src = "imgs/x-mark.png"
-        console.log('false')
     }
 }
 
