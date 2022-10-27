cf621558d9503570413c6996bd7b97406099ed5c
Sharon Arana <shar_mdza@hotmail.com>
2022-10-27 10:24:22 -0500

    change import name in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import validator from '/validator.js'
+import validator from 'validator.js'
 //Validación de numero de caracteres, salto al siguiente input
 const inputs = document.querySelectorAll(".cardInput");
 
